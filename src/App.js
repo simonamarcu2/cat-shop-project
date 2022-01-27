f5d98c3eaeed5332ed5416b664f906541c3af4d1
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-01-27 15:44:00 +0000

    All fetch funcs done, CPOs set as state

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
-
-import { useState, React } from 'react';
-import './App.css';
+import { useState, React } from "react";
+import "./App.css";
 // import randomWords from './components/faker'
 import { name, commerce } from "faker";
 
-
 function App() {
-  const [CPOs, setCPOs] = useState([]);
-  const handleFetch = async (setCPOs) => {
+  //states
+  const [CPOs, setCPOs] = useState([]); //the big list of cats
+  const [hasLoaded, setHasLoaded] = useState(false); //have we got a list yet?
+
+  const handleFetch = async () => {
     try {
       let response = await fetch(
         "https://api.thecatapi.com/v1/images/search?limit=20"
       );
       let data = await response.json();
       let updatedData = await addFakeData(data);
-      console.log("Hello-fetchs")
       setCPOs(updatedData);
-      // setLoading(false);
+      setHasLoaded(true);
     } catch (error) {
-      // setError(error);
+      console.log(error);
     }
   };
 
   const addFakeData = (data) => {
-    console.log("Hello-add fake data first")
-
     for (let i = 0; i < data.length; i++) {
       const element = data[i];
       element["name"] = name.firstName();
       element["price"] = commerce.price();
     }
-    handleFetch(setCPOs)
-    console.log("Hello-add fake data to")
     return data;
   };
-  addFakeData(CPOs)
-  return (
-    <div className="container">
-      {/* <randomWords /> */}
-    </div>
-  );
+  if (hasLoaded === false) {
+    handleFetch();
+    console.log(CPOs);
+  }
+  return <div className="container"> </div>;
 }
 
 export default App;
