b897b3409df42c1a3dfe0b4d4769382fd151f5ea
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-01-28 11:04:52 +0000

    button: add/remove things from basket

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,25 @@
-const Card = ({ CPO, index, setCPOs, array }) => {
+import React from "react";
 
-    // const handleBasketSelection = (index, CPO, setCPO) => {
-    //     const tempArray =
-    // }
+const Card = ({ CPO, index, handleBasketSelection, array }) => {
+  let selectedClass = array[index].selected ? "isSelected" : "notSelected";
+  return (
+    <div className={selectedClass + " catCard"} key={"catCard" + index}>
+      <img
+        className="catPhoto"
+        src={CPO.url}
+        alt={"picture of" + CPO.name}
+      ></img>
+      <div className="catName">{CPO.name}</div>
+      <div className="catPrice">{CPO.price}</div>
+      <button
+        onClick={() => {
+          handleBasketSelection(array, index);
+        }}
+      >
+        Basket
+      </button>
+    </div>
+  );
+};
 
-    return (
-        <div className="catCard">
-        <img className="catPhoto" src={CPO.url} alt={"picture of" + CPO.name}></img>
-        <div className="catName">{CPO.name}</div>
-        <div className="catPrice">{CPO.price}</div>
-        </div>)
-}
-
-export default Card
+export default Card;
