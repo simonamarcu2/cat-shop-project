c11dbd1f40b29d211c2b557df4aea116a4fc3dc1
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-01-28 11:42:31 +0000

    getBasketTotal function added

--- src/App.js
+++ src/App.js
@@ -46,6 +46,18 @@ function App() {
     setCPOs(newArray);
   };
 
+  const getBasketTotal = () => {
+    console.log(CPOs.length);
+    let total = 0;
+    for (let i = 0; i < CPOs.length; i++) {
+      if (CPOs[i].selected === true) {
+        console.log("true");
+        total += parseInt(CPOs[i].price);
+      }
+    }
+    return total;
+  };
+
   return (
     <div className="container">
       <div id="cats">
